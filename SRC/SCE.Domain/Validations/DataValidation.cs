349d28369d5ac68129d75f2a80f50c9d14454000
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-05-14 16:53:29 -0300

    Criando a classe responsável por gerar as validações de datas.

---
+++ SRC/SCE.Domain/Validations/DataValidation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCE.Domain.Validations
+{
+    public class DataValidation
+    {
+        public static bool NaoPodeSerNula(DateTime? date)
+        {
+            if (date == null)
+                return true;
+            return false;
+        }
+
+        public static bool NaoPodeSerMaiorQueDataAtual(DateTime? date)
+        {
+            if (date == null || date > DateTime.Now)
+                return true;
+            return false;
+        }
+
+        public static bool NaoPodeSerMenorQueDataAtual(DateTime? date)
+        {
+            if (date == null || date < DateTime.Now)
+                return true;
+            return false;
+        }
+    }
+
+}
