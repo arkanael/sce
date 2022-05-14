d4f8e4d4fd6327e948d08979594c5f907ceb026c
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-05-14 16:55:06 -0300

    Acertando as propiedades da classe de produto

--- SRC/SCE.Domain/Entities/Produto.cs
+++ SRC/SCE.Domain/Entities/Produto.cs
@@ -6,16 +6,37 @@ using System.Threading.Tasks;
 
 namespace SCE.Domain.Entities
 {
-    internal class Produto
+    public class Produto
     {
-        public Guid Id { get; set; }
-        public string Nome { get; set; }
-        public Fabricante Fabricante { get; set; }
-        public string Modelo { get; set; }
+        
+
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Modelo { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public DateTime? DataUltimaAtualizacao { get; private set; }
+
+        public void Cadastrar(string nome, string modelo)
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+            Modelo = modelo;
+            DataCadastro = DateTime.Now;
+            DataUltimaAtualizacao = null;
+        }
+
+        public void Atualizar(Guid id, string nome, string modelo)
+        {
+            Id = id;
+            Nome = nome;
+            Modelo = modelo;
+            DataUltimaAtualizacao = DateTime.Now;
+        }
+
 
         public override string ToString()
         {
-            return $"{Nome} - {Fabricante.Nome}";
+            return $"{Nome} - {Modelo}";
         }
     }
 }
