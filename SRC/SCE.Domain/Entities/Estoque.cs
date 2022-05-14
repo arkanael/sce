54f76fdca2cd7fdf8562a74f54f2cf30b3255cf0
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-05-14 17:03:25 -0300

    Primeiros acertos para a classe de estoque

---
+++ SRC/SCE.Domain/Entities/Estoque.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCE.Domain.Entities
+{
+    public class Estoque
+    {
+        public Guid Id { get; set; }
+        public int EstoqueInicial { get; set; }
+
+        public string Observacao { get; set; }
+
+
+
+    }
+}
