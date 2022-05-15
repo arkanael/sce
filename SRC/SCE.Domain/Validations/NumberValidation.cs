06966306230c3bff9fb4244ba97b1c05f02e98a6
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-05-15 15:26:34 -0300

    Desenvolvida a classe para as validações númericas.

---
+++ SRC/SCE.Domain/Validations/NumberValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCE.Domain.Validations
+{
+    public static class NumberValidation
+    {
+        /// <summary>
+        /// Valida se a número inteiro passada é nulo ou não
+        /// </summary>
+        /// <param name="value">vakor que está sendo validado</param>
+        /// <returns>False caso não tenha falhado na validação</returns>
+        /// <returns>True caso tenha falhado na validação</returns>
+        public static bool InteiroNaoPodeSerNulo(int? value)
+        {
+            if (value == null)
+                return true;
+            return false;
+        }
+    }
+}
