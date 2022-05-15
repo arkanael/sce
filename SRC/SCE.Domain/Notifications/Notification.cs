df16a6522c15b4e7ade971371d75462d8d25e50f
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-05-15 09:49:01 -0300

    Classe de notificação.

---
+++ SRC/SCE.Domain/Notifications/Notification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCE.Domain.Notifications
+{
+    public class Notification
+    {
+        public Notification(string message, string propertyName)
+        {
+            Message = message;
+            PropertyName = propertyName;
+        }
+
+        public string Message { get; private set; }
+        public string PropertyName { get; private set; }
+    }
+}
