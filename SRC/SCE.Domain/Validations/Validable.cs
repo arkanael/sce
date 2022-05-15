3a7ddf66dc74e30cda9d592aa8be7748bb02f7f5
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-05-15 09:51:07 -0300

    Classe responsável por agregar as notificações

---
+++ SRC/SCE.Domain/Validations/Validable.cs
@@ -0,0 +1,36 @@
+﻿using SCE.Domain.Contracts.Notifications;
+using SCE.Domain.Contracts.Validations;
+using SCE.Domain.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCE.Domain.Validations
+{
+    public abstract class Validable : IValidate, INotification
+    {
+        private List<Notification> _notifications;
+
+        public Validable()
+        {
+            _notifications = new List<Notification>();
+        }
+
+        public IReadOnlyCollection<Notification> Notifications { get { return _notifications; } }
+
+        public void ClearNotifications() 
+        { 
+            _notifications.Clear(); 
+        }
+
+        public void AddNotification(string property, string message)
+        {
+            _notifications.Add(new Notification(property, message));
+        }
+
+        public abstract bool Validate();
+
+    }
+}
