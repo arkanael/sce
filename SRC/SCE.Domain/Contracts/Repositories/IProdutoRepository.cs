31384b15d879b45fa499ad2349b06839f09c7cf9
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-05-15 20:51:11 -0300

    Criada a inteface para persistir os dados do produto no banco de dados

---
+++ SRC/SCE.Domain/Contracts/Repositories/IProdutoRepository.cs
@@ -0,0 +1,20 @@
+﻿using SCE.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCE.Domain.Contracts.Repositories
+{
+    public interface IProdutoRepository
+    {
+        void Cadastrar(Produto produto);
+        void Atualizar(Produto produto);
+        List<Produto> BuscarPorId(Guid id);
+        List<Produto> BuscaPor(Expression<Func<Produto, bool>> where);
+        int Quantidade(Guid id);
+        int QuantidadePor(Expression<Func<Produto, bool>> where);
+    }
+}
