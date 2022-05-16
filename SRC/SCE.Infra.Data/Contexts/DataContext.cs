183219c6b3870fcdbeaaa0ef8c12a2bb34348fb7
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-05-15 22:37:25 -0300

    Dando inicio aos primeiros desenvolvimentos da camada de repositorio de dados

---
+++ SRC/SCE.Infra.Data/Contexts/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCE.Infra.Data.Contexts
+{
+    public abstract class DataContext
+    {
+        protected SqlConnection sqlConnection;
+        protected SqlCommand sqlCommand;
+        protected SqlDataReader sqlDataReader;
+        protected SqlTransaction sqlTransaction;
+
+        protected string query = string.Empty;
+        protected readonly string connectionString = "";
+
+        protected void OpenConnection()
+        {
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+        }
+
+        protected void CloseConnection()
+        {
+            if (sqlConnection != null)
+                sqlConnection.Close();
+        }
+
+    }
+}
---
+++ SRC/SCE.Infra.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,66 @@
+﻿using SCE.Domain.Contracts.Repositories;
+using SCE.Domain.Entities;
+using SCE.Infra.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCE.Infra.Data.Repositories
+{
+    public class ProdutoRepository : DataContext, IProdutoRepository
+    {
+        public void Cadastrar(Produto produto)
+        {
+            try
+            {
+                OpenConnection();
+
+                query = "Insert into Produto(Id, Nome, Modelo) values(@Id, @Nome, @Modelo)";
+
+                sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlCommand.Parameters.AddWithValue("@Id", produto.Id);
+                sqlCommand.Parameters.AddWithValue("@Nome", produto.Nome);
+                sqlCommand.Parameters.AddWithValue("@Modelo", produto.Modelo);
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception erro)
+            {
+
+                throw new Exception("Não foi possível efetuar o cadastro.");
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        public void Atualizar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Produto> BuscarPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+       
+        public List<Produto> BuscaPor(Expression<Func<Produto, bool>> where)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Quantidade(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int QuantidadePor(Expression<Func<Produto, bool>> where)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
