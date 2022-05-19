439547e14d190d20528e2428b8bcc7f61c0556ae
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-05-18 23:06:14 -0300

    desenvolvendo a ideia de objeto de valor

---
+++ SRC/SCE.Domain/ValuesObjects/Endereco.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCE.Domain.ValuesObjects
+{
+    public class Endereco
+    {
+        public Endereco(string logradouro, string bairro, string cidade, string estado)
+        {
+            Logradouro = logradouro;
+            Bairro = bairro;
+            Cidade = cidade;
+            Estado = estado;
+        }
+
+        public Endereco()
+        {
+
+        }
+
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        
+    }
+}
