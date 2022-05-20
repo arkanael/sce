4045470d21dc0f938aa796708ff799f80749ce1e
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-05-20 00:52:21 -0300

    Desenvolvida a entidade base responsável por manipular o id e as datas via herança

---
+++ SRC/SCE.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,38 @@
+﻿using SCE.Domain.Contracts.Entities;
+using SCE.Domain.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCE.Domain.Entities
+{
+    public abstract class BaseEntity<T> : Validable where T : IEntity
+    {
+        protected BaseEntity()
+        {
+            if (Id == null)
+            {
+                Id = Guid.NewGuid();
+            }
+        }
+
+        public Guid? Id { get; private set; }
+
+        public DateTime DataCadastro { get; private set; }
+
+        public DateTime? DataUltimaAtualizacao { get; private set; }
+
+        public void SetDataCadastrado()
+        {
+            DataCadastro = DateTime.Now;
+        }
+
+        public void SetDataAtualizacao()
+        {
+            DataCadastro = DateTime.Now;
+        }
+
+    }
+}
