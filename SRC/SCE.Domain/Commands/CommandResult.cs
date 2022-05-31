a73b7ccb211a3b47f7960c20f7f3ea01dd8729d5
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-05-30 23:38:45 -0300

    Desenvolvida a logica para a resposta de um comando.

---
+++ SRC/SCE.Domain/Commands/CommandResult.cs
@@ -0,0 +1,19 @@
+﻿using SCE.Domain.Contracts.Commands;
+using SCE.Domain.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCE.Domain.Commands
+{
+    public class CommandResult : ICommandResult
+    {
+        public bool Ok { get; set; }
+        public string PropertyName { get; set; }
+        public string Message { get; set; }
+
+        public IReadOnlyCollection<Notification> Notifications { get; set; }
+    }
+}
