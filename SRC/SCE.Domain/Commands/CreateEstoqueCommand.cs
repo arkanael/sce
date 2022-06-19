3fc24e205fa33b798ce6f6be6df6a3fbe107e923
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-06-19 17:47:57 -0300

    Criando o command do estoque

---
+++ SRC/SCE.Domain/Commands/CreateEstoqueCommand.cs
@@ -0,0 +1,24 @@
+﻿using SCE.Domain.Contracts.Commands;
+using SCE.Domain.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCE.Domain.Commands
+{
+    public class CreateEstoqueCommand: Validable, ICommand
+    {
+        public String Produto { get; set; }
+        public int EstoqueInicial { get; set; }
+        public int Entrada { get; set; }
+        public int Saida { get; set; }
+        public int Quantidade { get; set; }
+
+        public override bool Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
